277391ab9a650ec7ebc3640f6dd6f7fefeb23a68
Kasper Lewau <lewauu@gmail.com>
2018-05-01 14:17:44 +0200

    darken

--- config.h
+++ config.h
@@ -84,31 +84,25 @@ static unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 const char *colorname[] = {
-
-  /* 8 normal colors */
-  /* [0] = "#282c34", /1* black   *1/ */
-  [0] = "#222222",
-  [1] = "#e06c75", /* red     */
-  [2] = "#98c379", /* green   */
-  [3] = "#e5c07b", /* yellow  */
-  [4] = "#61afef", /* blue    */
-  [5] = "#c678dd", /* magenta */
-  [6] = "#56b6c2", /* cyan    */
-  [7] = "#abb2bf", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#3e4452", /* black   */
-  [9]  = "#be5046", /* red     */
-  [10] = "#98c379", /* green   */
-  [11] = "#d19a66", /* yellow  */
-  [12] = "#61afef", /* blue    */
-  [13] = "#c678dd", /* magenta */
-  [14] = "#56b6c2", /* cyan    */
-  [15] = "#5c6370", /* white   */
-
-  /* special colors */
-  [256] = "#282c34", /* background */
-  [257] = "#abb2bf", /* foreground */
+  /* [0] = "#222222", */
+	[0] = "#151515",
+	[1] = "#e06c75", /* red     */
+	[2] = "#98c379", /* green   */
+	[3] = "#e5c07b", /* yellow  */
+	[4] = "#61afef", /* blue    */
+	[5] = "#c678dd", /* magenta */
+	[6] = "#56b6c2", /* cyan    */
+	[7] = "#abb2bf", /* white   */
+	[8]  = "#3e4452", /* black   */
+	[9]  = "#be5046", /* red     */
+	[10] = "#98c379", /* green   */
+	[11] = "#d19a66", /* yellow  */
+	[12] = "#61afef", /* blue    */
+	[13] = "#c678dd", /* magenta */
+	[14] = "#56b6c2", /* cyan    */
+	[15] = "#5c6370", /* white   */
+	[256] = "#282c34", /* background */
+	[257] = "#abb2bf", /* foreground */
 };
 
 /*
